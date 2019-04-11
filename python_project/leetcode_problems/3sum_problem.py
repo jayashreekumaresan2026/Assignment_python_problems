ca77297b5c221d409da63ab4976d5c3570c2f446
jayashreekumaresan2026 <hacker_rank_problems/beautiful_days_at_movie.py>
2019-04-11 22:30:24 +0530

    second week assignment

--- python_project/leetcode_problems/3sum_problem.py
+++
@@ -1,9 +0,0 @@
-# https://leetcode.com/problems/3sum/
-# def three_sum_problem(user_input):
-#     for i in range(0,len(user_input)):
-#         for j in range(i+1,len(user_input)):
-#
-#
-#
-# user_input = [-1, 0, 1, 2, -1, -4]
-# three_sum_problem(user_input)
