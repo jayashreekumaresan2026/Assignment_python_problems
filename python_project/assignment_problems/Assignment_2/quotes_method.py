2a42139babdffea406f9d113587f19c8c3a8561f
jayashreekumaresan2026 <hacker_rank_problems/beautiful_days_at_movie.py>
2019-04-26 23:19:47 +0530

    added functionality for method in python

--- python_project/assignment_problems/Assignment_2/quotes_method.py
+++ python_project/assignment_problems/Assignment_2/quotes_method.py
@@ -4,27 +4,23 @@ from time import sleep
 import xmltodict as xmltodict
 
 
-def print_quotes_for_30m():
+def print_quotes_in_delay():
     while True:
         with open("config.json") as jsonfile:
             jsoncontent = json.load(jsonfile)
             delay_time = int(jsoncontent["delay"])
             user_language = ["en", "ru"]
-            if jsoncontent["lang"] in user_language:
-                if jsoncontent["format"] == "json":
-                    response = reqs.request(method="GET",
+            response = reqs.request(method="GET",
                                             url="https://api.forismatic.com/api/1.0/?method=getQuote&lang={key}&format={format}".format(
                                                 key=jsoncontent["lang"], format=jsoncontent["format"]))
-                    text_recieved = response.text
+            text_recieved = response.text
+            if jsoncontent["lang"] in user_language:
+                if jsoncontent["format"] == "json":
                     quoteObj = json.loads(bytes(text_recieved, "utf-8").decode("unicode_escape"))
                     author, quotes = extract_the_text(quoteObj)
                     print("Author :" + author + "   Says:" + quotes)
                     sleep(delay_time)
                 else:
-                    response = reqs.request(method="GET",
-                                            url="https://api.forismatic.com/api/1.0/?method=getQuote&lang={key}&format={format}".format(
-                                                key=jsoncontent["lang"], format=jsoncontent["format"]))
-                    text_recieved = response.text
                     xmltodicts = json.loads(json.dumps(xmltodict.parse(text_recieved)))
                     quote_text = xmltodicts["forismatic"]['quote']
                     if quote_text["quoteAuthor"] == None:
@@ -44,9 +40,9 @@ def print_quotes_for_30m():
                                             key=language, format=format))
                 text_recieved = response.text
                 quoteObj = json.loads(bytes(text_recieved, "utf-8").decode("unicode_escape"))
-                quotes, author = extract_the_text(quoteObj)
-                print("Author :" + quotes + "says" + author)
-            sleep(delay_time)
+                author, quotes = extract_the_text(quoteObj)
+                print("Author :" + author + "Says  " + quotes)
+                sleep(delay_time)
 
 def extract_the_text(the_text_in_dictionary):
     author_name = the_text_in_dictionary["quoteAuthor"]
@@ -58,4 +54,4 @@ def extract_the_text(the_text_in_dictionary):
         return author_name, quote_message
 
 
-print_quotes_for_30m()
+print_quotes_in_delay()
