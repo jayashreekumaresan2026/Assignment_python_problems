e14207dce15777cb08db9583ce45bea90dfad172
jayashreekumaresan2026 <hacker_rank_problems/beautiful_days_at_movie.py>
2019-04-29 15:18:38 +0530

    added timing functionality to the program

---
+++ python_project/assignment_problems/Assignment_2/test_case_calling_file.py
@@ -0,0 +1,35 @@
+def test_case1():
+    test_Case1 = "test_case/config.json"
+    return test_Case1
+
+
+def test_case2():
+    test_Case2 = "test_case/config2.json"
+    return test_Case2
+
+
+def test_case3():
+    test_Case3 = "test_case/config3.json"
+
+    return test_Case3
+
+
+
+def test_case4():
+    test_Case4 = "test_case/config4.json"
+
+    return test_Case4
+
+
+
+def test_case5():
+    test_Case5 = "test_case/config5.json"
+
+    return test_Case5
+
+
+def test_case6():
+    test_Case6 = "test_case/config6.json"
+
+    return test_Case6
+
