afe33bf59dfedf649ce58be5d33f0144cf8bbc5d
jayashreekumaresan2026 <hacker_rank_problems/beautiful_days_at_movie.py>
2019-04-30 07:17:37 +0530

    added different timing funtionality

--- python_project/assignment_problems/Assignment_2/quotes_another_method.py
+++ python_project/assignment_problems/Assignment_2/quotes_another_method.py
@@ -5,11 +5,32 @@ from time import sleep
 import xmltodict as xmltodict
 
 
-def print_quotes_for_30m():
+def print_quotes_for_time(test_Case):
     while True:
-        with open("config.json") as jsonfile:
+        with open(test_Case) as jsonfile:
             jsoncontent = json.load(jsonfile)
-            delay_time = int(jsoncontent["delay"])
+            user_input= jsoncontent["delay"]
+            new_time_list = []
+            total_seconds = 0
+            append_to_the_list = user_input.split()
+            print(append_to_the_list)
+            for i in range(len(append_to_the_list)):
+                check_the_text = append_to_the_list[i]
+                extract_the_text = check_the_text[-1]
+                extract_the_value = check_the_text[:-1]
+                print(extract_the_text)
+                print(extract_the_value)
+                if extract_the_text == 'y':
+                    total_seconds = int(extract_the_value) * 365 * 24 * 60 * 60
+                    new_time_list.append(total_seconds)
+                elif extract_the_text == 'h':
+                    total_seconds += int(extract_the_value) * 3600
+                    new_time_list.append(total_seconds)
+                elif extract_the_text == 'm':
+                    total_seconds += int(extract_the_value) * 60
+                else:
+                    total_seconds += int(extract_the_value)
+            print(total_seconds)
             user_language = ["en", "ru"]
             if jsoncontent["lang"] in user_language:
                 if jsoncontent["format"] == "json":
@@ -17,7 +38,7 @@ def print_quotes_for_30m():
                     format=jsoncontent["format"]
                     author, quotes = common_response(language, format)
                     print("Author :" + author + "says" + quotes)
-                    sleep(delay_time)
+                    sleep(total_seconds)
                 else:
                     response = reqs.request(method="GET",
                                             url="https://api.forismatic.com/api/1.0/?method=getQuote&lang={key}&format={format}".format(
@@ -30,14 +51,14 @@ def print_quotes_for_30m():
                         print("AUTHOR: {} SAYS: {}".format(quote_text["quoteAuthor"], quote_text["quoteText"]))
                     else:
                         print("AUTHOR: {} SAYS: {}".format(quote_text["quoteAuthor"], quote_text["quoteText"]))
-                    sleep(delay_time)
+                    sleep(total_seconds)
 
             else:
                 language = "en"
                 format = "json"
                 author,quotes=common_response(language,format)
                 print("Author :" + author+ "says" + quotes)
-                sleep(delay_time)
+                sleep(total_seconds)
 def common_response(language,format):
         response = reqs.request(method="GET",
                                 url="https://api.forismatic.com/api/1.0/?method=getQuote&lang={key}&format={format}".format(
@@ -57,5 +78,11 @@ def extract_the_text(the_text_in_dictionary):
     else:
         return author_name, quote_message
 
+test_Case1 = "test_case/config.json"
+test_Case2 = "test_case/config2.json"
+test_Case3 = "test_case/config3.json"
+test_Case4 = "test_case/config4.json"
+test_Case5 = "test_case/config5.json"
+test_Case6 = "test_case/config6.json"
 
-print_quotes_for_30m()
+print_quotes_for_time(test_Case1)
