c4f19aa69fa29aebeceac688e17abf69be2e5b49
jayashreekumaresan2026 <hacker_rank_problems/beautiful_days_at_movie.py>
2019-04-30 16:44:41 +0530

    simplified functionality in the code

--- python_project/assignment_problems/Assignment_2/quotes_another_method.py
+++ python_project/assignment_problems/Assignment_2/quotes_another_method.py
@@ -6,63 +6,70 @@ import xmltodict as xmltodict
 
 
 def open_the_json_file(test_Case):
+    # file=open(test_Case,"r")
+    # jsoncontent = json.loads(jsoncontent)
+    # file.close()
     with open(test_Case) as jsonfile:
-        jsoncontent = json.load(jsonfile)
-        language = jsoncontent["lang"]
-        format = jsoncontent["format"]
-        # timing must be in the format for year /y ,hour/h,minutes/m,seconds/s specify space timing between the timing
-        user_input = jsoncontent["delay"]
-        total_seconds = timing_spit(user_input)
-        print_quotes_for_time(jsoncontent, total_seconds, language, format)
+        jsoncontent = jsonfile.read()
+    print(jsonfile.closed)
+    jsoncontent = json.loads(jsoncontent)
+    language = jsoncontent["lang"]
+    format = jsoncontent["format"]
+    # timing must be in the format for year /y ,hour/h,minutes/m,seconds/s specify space timing between the timing
+    user_input = jsoncontent["delay"]
+    total_seconds = timing_split(user_input)
+    print_quotes_for_time(jsoncontent, total_seconds, language, format)
 
 
 def print_quotes_for_time(jsoncontent, total_seconds, language, format):
     while True:
         user_language = ["en", "ru"]
-        if jsoncontent["lang"] not in user_language:
-            jsoncontent["lang"] = "en"
-            jsoncontent['format'] = "json"
+        if language not in user_language:
+            language = "en"
+            format = "json"
         if jsoncontent["lang"] in user_language:
-            if jsoncontent["format"] == "json":
-                author, quotes = common_response(jsoncontent["lang"], format)
-                print("Author :" + author + "says" + quotes)
+            if format == "json":
+                author, quotes = common_response(language, total_seconds, jsoncontent)
+                print("Author :" + author + " says:" + quotes)
                 sleep(total_seconds)
             else:
-                response = reqs.request(method="GET",
-                                        url="https://api.forismatic.com/api/1.0/?method=getQuote&lang={key}&format={format}".format(
-                                            key=jsoncontent["lang"], format=jsoncontent["format"]))
-                text_recieved = response.text
+                text_recieved = extract_quotes_from_site(jsoncontent)
                 xmltodicts = json.loads(json.dumps(xmltodict.parse(text_recieved)))
                 quote_text = xmltodicts["forismatic"]['quote']
                 if quote_text["quoteAuthor"] == None:
-                    quote_text["quoteAuthor"] = "UNKNOWN_AUTHOR"
-                    print("Author: {} Says: {}".format(quote_text["quoteAuthor"], quote_text["quoteText"]))
+                    to_print_the_resultant_quotes( quote_text["quoteText"], total_seconds)
                 else:
                     print("Author: {} Says: {}".format(quote_text["quoteAuthor"], quote_text["quoteText"]))
                 sleep(total_seconds)
 
 
-def common_response(language, format):
-    response = reqs.request(method="GET",
-                            url="https://api.forismatic.com/api/1.0/?method=getQuote&lang={key}&format={format}".format(
-                                key=language, format=format))
-    text_recieved = response.text
-    quoteObj = json.loads(bytes(text_recieved, "utf-8").decode("unicode_escape"))
-    author, quotes = extract_the_text(quoteObj)
+def to_print_the_resultant_quotes(quotes, total_seconds):
+    author_name = "UNKNOWN_AUTHOR"
+    print("Author :" + author_name + " says :" + quotes)
+    sleep(total_seconds)
+
+
+def common_response(language, total_seconds, jsoncontent):
+    text_recieved = extract_quotes_from_site(jsoncontent)
+    if language == "en":
+        quoteObj = json.loads(bytes(text_recieved, "utf-8").decode("unicode_escape"))
+        author, quotes = extract_the_text(quoteObj, total_seconds)
+    else:
+        quotes_obj = json.loads(text_recieved)
+        author, quotes = extract_the_text(quotes_obj, total_seconds)
     return author, quotes
 
 
-def extract_the_text(the_text_in_dictionary):
-    author_name = the_text_in_dictionary["quoteAuthor"]
-    quote_message = the_text_in_dictionary["quoteText"]
+def extract_the_text(text_recieved, total_seconds):
+    author_name = text_recieved["quoteAuthor"]
+    quote_message = text_recieved["quoteText"]
     if author_name == '':
-        author_name = "UNKNOWN_AUTHOR"
-        return author_name, quote_message
+        to_print_the_resultant_quotes(author_name, quote_message, total_seconds)
     else:
         return author_name, quote_message
 
 
-def timing_spit(user_input):
+def timing_split(user_input):
     new_time_list = []
     total_seconds = 0
     append_to_the_list = user_input.split()
@@ -81,6 +88,14 @@ def timing_spit(user_input):
         else:
             total_seconds += int(extract_the_value)
     return total_seconds
+
+
+def extract_quotes_from_site(jsoncontent):
+    response = reqs.request(method="GET",
+                            url="https://api.forismatic.com/api/1.0/?method=getQuote&lang={key}&format={format}".format(
+                                key=jsoncontent["lang"], format=jsoncontent["format"]))
+    text_recieved = response.text
+    return text_recieved
 
 
 test_Case1 = "test_case/config.json"
